5d82282791df531015032d967567ca2fbb8b12c9
Kenny <kenny.romero@zavgroup.com>
2017-06-12 11:45:44 -0500

    inital repo

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+
+import './App.css';
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
+  componentWillMount (){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({user});
+    })
+  }
+
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+    .catch(error => console.log(`Error ${error.code} : ${error.message} `));
+  }
+
+  renderLoginButton(){
+    if(this.state.user) {
+      return (
+        <p>
+          <div>hola</div>
+        </p>
+      );
+    }else{
+      <button onClick={this.handleAuth}>Login Google</button>
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+            <h2>KeeperZav</h2>
+        </div>
+        <p className="App-intro">
+        {this.renderLoginButton()};
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAFl9OOkHr_PVpdQz5NdlEoYhYIZB0bgI4",
+	authDomain: "react-example-a9b80.firebaseapp.com",
+	databaseURL: "https://react-example-a9b80.firebaseio.com",
+	projectId: "react-example-a9b80",
+	storageBucket: "react-example-a9b80.appspot.com",
+	messagingSenderId: "794811170116"
+};
+firebase.initializeApp(config);
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root'));
+registerServiceWorker();
